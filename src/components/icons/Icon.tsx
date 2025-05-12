6bc0addcda43c0c386b719460040bccb5562f95e
vodyaraman <130937079+vodyaraman@users.noreply.github.com>
2025-05-12 20:04:39 +0300

    Small fix

--- src/components/icons/Icon.tsx
+++ src/components/icons/Icon.tsx
@@ -4,7 +4,7 @@ import type { IconProps } from '@/types/components.interfaces';
 
 export default function Icon({ id, className }: IconProps) {
     return (
-        <svg className={clsx("icon-sprite", className)} viewBox="0 0 24 24">
+        <svg className={clsx("icon-sprite", className)}>
             <use href={`#${id}`} />
         </svg>
     )
